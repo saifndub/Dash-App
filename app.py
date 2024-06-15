7ea101c322b29ff2763fd870ab5c6c421c6bbc2e
Md Saif Hossain <saif.ndub@gmail.com>
2024-06-15 19:04:44 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# My first app
+Hello *world!*
+""")
+ 
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
